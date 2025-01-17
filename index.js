5a1b18bff8ea8badda47a3a801ad10130347f4e4
Vibhu Mishra <vibhumishra14@gmail.com>
2025-01-17 19:44:56 +0530

    foundationial data completion part 1

--- index.js
+++ index.js
@@ -3,7 +3,55 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 
+const { getAllStocks, getStockByTicker, addNewTrade, validateTrade } = require('./investFlow.js');
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Invest Flow - Stock Trading Project");
+});
+
+app.get('/stocks', async (req, res) => {
+    try {
+        let result = await getAllStocks();
+        if (result.stocks.length === 0) {
+            return res.status(404).json({message: "No Stocks Found!"});
+        }
+        return res.status(200).json(result);
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+
+    }
+});
+
+app.get('/stocks/:ticker', async (req, res) => {
+    try {
+        let ticker = req.params.ticker;
+        if (typeof ticker === 'string') {
+            return res.status(400).json({message: "Ticker should be a string."});
+        }
+        let result = await getStockByTicker(ticker);
+        if (result.stock.length === 0) {
+            return res.status(404).json({message: "No Stock Found!"})
+        }
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+    }
+})
+
+app.post('/trades/new', async (req, res) => {
+    try {
+        let newTrade = req.body;
+        let error = validateTrade(newTrade);
+        if (error) res.status(400).json({message: error});
+        let result = addNewTrade(newTrade);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+app.post('/trades/new')
 
 module.exports = { app };
---
+++ investFlow.js
@@ -0,0 +1,42 @@
+let stocks = [
+    { stockId: 1, ticker: 'AAPL', companyName: 'Apple Inc.', price: 150.75 },
+    { stockId: 2, ticker: 'GOOGL', companyName: 'Alphabet Inc.', price: 2750.10 },
+    { stockId: 3, ticker: 'TSLA', companyName: 'Tesla, Inc.', price: 695.50 },
+  ];
+  
+  let trades = [
+    { tradeId: 1, stockId: 1, quantity: 10, tradeType: 'buy', tradeDate: '2024-08-07' },
+    { tradeId: 2, stockId: 2, quantity: 5, tradeType: 'sell', tradeDate: '2024-08-06' },
+    { tradeId: 3, stockId: 3, quantity: 7, tradeType: 'buy', tradeDate: '2024-08-05' },
+  ];
+  
+async function getAllStocks() {
+    return {stocks: stocks};
+}
+
+async function getStockByTicker(ticker) {
+    return {stock: stocks.find((stock) => stock.ticker === ticker)};
+}
+
+async function addNewTrade(newTrade) {
+    trades.push({id: trades.length + 1, ...newTrade});
+    return {newTrade: newTrade};
+}
+
+function validateTrade(newTrade) {
+    if (!newTrade.stockId || typeof newTrade.stockId !== 'number' || newTrade.stockId < 0) {
+        return "Stock Id is required and should be a positive number.";
+    }
+    if (!newTrade.quantity || typeof newTrade.quantity !== 'number' || newTrade.quantity < 0) {
+        return "Quantity is required and should be a positive number.";
+    }
+    if (!newTrade.tradeType || typeof newTrade.tradeType !== 'string' || newTrade.tradeType === 'buy' || newTrade.tradeType === 'sell') {
+        return "Trade Type is required and should be a 'buy' or 'sell' string."
+    }
+    if (!newTrade.tradeDate || isNaN(Date.parse(newTrade.tradeDate))) {     // When you apply parse method over 'Date' object and if it is valid then output would be returned in number
+        return "Trade date is required and should be 'yyyy-mm-dd' format.";
+    }
+    return null;
+}
+
+module.exports = { getAllStocks, getStockByTicker, validateTrade, addNewTrade }
